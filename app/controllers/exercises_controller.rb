e06914060a9bab4650190a65417f826d26ab2d48
javante_nelson <javantenelson@gmail.com>
2020-08-10 18:15:26 -0500

    render json for api in both controllers

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -2,18 +2,28 @@ class ExercisesController < ApplicationController
 
     
     def index
+        exercises = Exercise.all
+        render json: exercises
     end
 
     def create
+        exercise = Exercise.create(exercise_params)
     end
 
     def show
+        exercise = Exercise.find_by(id: params[:id])
+        render json: exercise
     end
 
     def update
+        exercise = Exercise.find_by(id: params[:id])
+        exercise.update(exercise_params)
+        render json: exercise
     end
 
     def destroy
+        exercise = Exercise.find_by(id: params[:id])
+        exercise.destroy
     end
 
 end
--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,17 +1,28 @@
 class MealsController < ApplicationController
 
     def index
+        meals = Meal.all
+        render json: meals
     end
 
     def create
+        meal = Meal.create(meal_params)
+        
     end
 
     def show
+        meal = Meal.find_by(id: params[:id])
+        render json: meal
     end
 
     def update
+        meal = Meal.find_by(id: params[:id])
+        meal.update(meal_params)
+        render json: meal
     end
 
     def destroy
+        meal = Meal.find_by(id: params[:id])
+        meal.destroy
     end
 end
